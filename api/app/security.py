b6bbe67194ba54f0b76403d83eb306216fe40b82
Vincent Mousseaux <mousseaux.v@gmail.com>
2025-10-29 19:13:09 +0100

    Feature: Add api_key for shorte_code

---
+++ api/app/security.py
@@ -0,0 +1,14 @@
+import os
+from fastapi import Security, HTTPException, status
+from fastapi.security import APIKeyHeader
+
+API_KEY = os.getenv("API_KEY")
+API_KEY_NAME = "X-API-KEY"
+api_key_header = APIKeyHeader(name=API_KEY_NAME, auto_error=False)
+
+async def get_api_key(api_key: str = Security(api_key_header)):
+    if api_key == API_KEY:
+        return api_key
+    else:
+       raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN, detail="Could not validate credentials")
